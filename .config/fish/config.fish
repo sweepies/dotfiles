60991925966f06219d7507b3273089c249acb692
Sweepyoface <sweepyoface@gmail.com>
2020-12-05 02:18:33 -0800

    try out fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+if [ -z $SSH_CONNECTION ] && type code-insiders &> /dev/null
+    export EDITOR="code-insiders"
+    alias code="code-insiders"
+else
+    export EDITOR="nano"
+end
+
+# WSL specific
+if grep --quiet --extended-regexp --ignore-case "(Microsoft|WSL)" /proc/version
+    function explorer
+        if [ -z $1 ]
+            explorer.exe (wslpath -w .)
+        else
+            explorer.exe (wslpath -w $1)
+        end
+    end
+    alias explore=explorer
+end
+
+check_updates
