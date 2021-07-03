36c1c9304de4c8b784d162f224e0d73d770601f2
Sweepyoface <sweepy@sweepy.dev>
2021-07-03 16:23:38 -0700

    init

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,2 @@
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
---
+++ private_dot_config/private_fish/functions/rand.fish
@@ -0,0 +1,48 @@
+#!/usr/bin/env fish
+
+function rand
+    set types "hex" "base64" "b64" "chars" "c" "password" "pw"
+
+    if set -q $argv[1]
+        printf "Usage: %s TYPE [<AMOUNT>]\n" (status current-command)
+        printf "\
+Generate random data
+
+Possible types:
+  hex
+  base64, b64
+  chars, c
+  password, pw
+
+When using encoding types, AMOUNT is in bytes.\n"
+        return 126
+    end
+
+    if not contains $argv[1] $types
+        printf "%s: invalid type: '%s'\n" (status current-command) "$argv[1]"
+        return 22
+    end
+
+    if not string match -q $argv[1] "password" "pw"
+        if not string match -rq '^[0-9]+$' $argv[2]
+            printf "%s: invalid number: '%s'\n" (status current-command) "$argv[2]"
+            return 22
+        end
+    end
+
+
+    switch $argv[1]
+        case "hex"
+            set data (head -c "$argv[2]" < /dev/urandom | xxd -ps | tr -d '\n')
+        case "base64" "b64"
+            set data (head -c "$argv[2]" < /dev/urandom | base64)
+        case "chars" "c"
+            set data (tr -cd "[:alnum:]" < /dev/urandom | head -c "$argv[2]")
+        case "password" "pw"
+            #set data (tr -cd "[:alnum:]" < /dev/urandom | head -c 24)
+            set data (rand chars 24)
+
+    end
+
+    printf "%s\n" "$data"
+end
